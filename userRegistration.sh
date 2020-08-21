750aa44ee29028a0ff72ac83f17ba04d3546e170
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-21 11:46:29 +0530

    Password validation - at least one numeric number

--- userRegistration.sh
+++ userRegistration.sh
@@ -32,7 +32,7 @@ fi
 
 
 read -p "Enter your password : " password
-if [[ ${#password} -ge 8 ]] && [[ "$password" == *[[:upper:]]* ]]
+if [[ ${#password} -ge 8 ]] && [[ "$password" == *[[:upper:]]* && "$password" == *[[:lower:]]* && "$password" == *[0-9]* ]]
 then
 	echo "Password is correct"
 else
