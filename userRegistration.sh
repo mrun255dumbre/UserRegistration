6d7fc5441d8b5394ba928145383c774e606c4499
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-21 10:50:03 +0530

    Email Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,7 @@
 echo "User Registration"
 
 patternName="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+patternEmail="^([a-z0-9\_\-\+]+[\.]{0,1}[a-z0-9]+)@([a-z0-9]+)([\.]{0}[a-z]{0}|[\.]{1}[a-z]{2,5})\.([a-z]{2,5})$"
 
 read -p "Enter your name : " name lastName
 
@@ -9,4 +10,13 @@ then
 	echo "First name and Last name is correct";
 else
 	echo "Please enter valid Name"
+fi
+
+
+read -p "Enter your email : " email
+if [[ $email =~ $patternEmail ]]
+then
+	echo "Email is correct"
+else
+	echo "Please enter valid Email"
 fi
