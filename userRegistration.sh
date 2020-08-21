4c98eac2ab845c03aa61f070e533b43f967f0938
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-21 10:26:45 +0530

    first name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,12 @@
 echo "User Registration"
 
+patternName="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+
+read -p "Enter your name : " name
+
+if [[ $name =~ $patternName ]]
+then
+	echo "First name is correct";
+else
+	echo "Please enter valid Name"
+fi
